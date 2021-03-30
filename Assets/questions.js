66e380809050353078fe78ea93befafc17d0bc94
CGalko <cash.galko12@ncf.edu>
2021-03-30 17:50:18 -0400

    Created the Assets folder. Added questions.js which contains an array of all questions & answers for the quiz.

---
+++ Assets/questions.js
@@ -0,0 +1,54 @@
+// local storage to store / clear highscores
+
+// Using display settings in css to show / hide questions
+
+// On click events to compare correct and incorrect answers
+
+// This is an array of Objects.
+// Use this notation to pull info from objects.
+console.log(questions[0].choice[3]);
+
+var questions = [
+    {
+        title: "Which of the following provides free source code for widgets for use on our webpages?",
+        choice: ["Javascript", "Getbootstrap", "jQuery", "Github"],
+        answer: "jQuery"
+    },
+
+    {
+        title: "jQuery Elements, Getbootstrap.com & jQuery UI are examples of what?",
+        choice: ["Javascript", "Third Party APIs", "CSS", "HTML"],
+        answer: "Third Party APIs"
+    },
+
+    {
+        title: "Which method is used to make an HTML element appear on the page in Javascript?",
+        choice: [".createElement", ".setAttribute", ".addEventListener", ".appendChild"],
+        answer: ".appendChild"
+    },
+
+    {
+        title: "What would you use to create hover effects using CSS?",
+        choice: ["Pseudo-Classes", "Pseudo-Elements", "Wireframing", "Selectors"],
+        answer: "Pseudo-Classes"
+    },
+
+    {
+        title: "Which of the following can be used to style a webpage's layout?",
+        choice: ["HTML", "Javascript", "CSS", "All of the above"],
+        answer: "All of the above"
+    },
+
+    {
+        title: "What does DOM stand for?",
+        choice: ["Document Object Model", "Delibrate Operation Movement", "Directional Orientation of Movement", "Div Object Memory"],
+        answer: "Document Object Model"
+    },
+
+    {
+        title: "Which of the following is a scripting language?",
+        choice: ["VSCode", "Javascript", "HTML", "CSS"],
+        answer: "Javascript"
+    },
+
+]
